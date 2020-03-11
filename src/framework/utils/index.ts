003aa31c2edfb5761b6b0cbcb414a62d544e5269
Anoop2403 <anoopvarghese653264@gmail.com>
2020-03-11 16:47:40 +0530

    1 file modified, 3 added

--- src/framework/utils/index.ts
+++ src/framework/utils/index.ts
@@ -2,5 +2,5 @@ export * from './validators/custom-validators.service';
 export * from './form-modal-converter/form-modal-converter.service';
 export * from './multikeytranslate.pipe';
 export * from './file-util.service';
-export * from './animation/animation-constant';
+export * from '/animation/animation-constant';
 
